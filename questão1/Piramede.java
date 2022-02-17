6ae2d4913288777ba0e5d3fd9e9bfa8d7fc97280
Aleviannaf <alexandrevianna511@gmail.com>
2022-02-17 17:27:47 -0400

    questao 1

---
+++ questão1/Piramede.java
@@ -0,0 +1,29 @@
+package capgemini.questão1;
+
+public class Piramede {
+    private int entrada;
+
+    Piramede(int entrada){
+        this.entrada = entrada;
+    }
+
+    public void setEntrada(int entrada) {
+        this.entrada = entrada;
+    }
+
+    public void montarPiramede(){
+        for(int linha = 0; linha<entrada; linha++){
+            for(int space=entrada; space>linha; --space){
+                System.out.print(" ");
+            }
+
+            for(int asterisc = 0; asterisc<=linha; asterisc++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
+
+
---
+++ questão1/questao1Main.java
@@ -0,0 +1,9 @@
+package capgemini.questão1;
+
+public class questao1Main {
+    public static void main(String[] args){
+        Piramede teste = new Piramede(6);
+
+        teste.montarPiramede();
+    }
+}
